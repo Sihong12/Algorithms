467276ff731c2ce6c233c422ece3ec5e08c54cc7
Sihong Park <pozt1234@gmail.com>
2017-10-09 18:31:18 +0900

    Solve problem

--- src/BOJ/DP/Problem_11053.java
+++ src/BOJ/DP/Problem_11053.java
@@ -1,6 +1,9 @@
 package BOJ.DP;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 /**
  * 가장 긴 증가하는 부분 수열
@@ -11,15 +14,17 @@ import java.util.Scanner;
  * 2. O(N^2)과 O(NlogN) 두 가지 방법이 존재, 여기선 O(N^2)을 사용.
  */
 public class Problem_11053 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
         int[] input = new int[n+1];
         int[] DP = new int[n+1];
 
+        StringTokenizer tokens = new StringTokenizer(br.readLine(), " ");
+
         int max = 0;
         for(int i=1; i<=n; i++) {
-            input[i] = scanner.nextInt();
+            input[i] = Integer.parseInt(tokens.nextToken());
             DP[i] = 1;
             for(int j=1; j<i; j++) {
                 if(input[j] < input[i] && DP[j] + 1 > DP[i])
@@ -27,5 +32,6 @@ public class Problem_11053 {
             }
             max = Math.max(max, DP[i]);
         }
+        System.out.println(max);
     }
 }
---
+++ src/BOJ/DP/Problem_11053_nlogn.java
@@ -0,0 +1,61 @@
+package BOJ.DP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 가장 긴 증가하는 부분 수열
+ *  수열 A가 주어졌을 때, 가장 긴 증가하는 부분 수열의 길이를 출력하라.
+ *
+ * <해결>
+ * 1. LIS(Longest Increasing Subsequence) 알고리즘을 사용.
+ * 2. O(N^2)과 O(NlogN) 두 가지 방법이 존재, 여기선 O(NlogN)을 사용.
+ */
+public class Problem_11053_nlogn {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer tokens = new StringTokenizer(br.readLine(), " ");
+        int[] input = new int[n];
+        for(int i=0; i<n; i++) {
+            input[i] = Integer.parseInt(tokens.nextToken());
+        }
+
+        int[] subsequence = new int[n];
+        subsequence[0] = input[0];
+        int length = 1;
+        for(int i=1; i<n; i++) {
+            if(input[i] < subsequence[0]) {
+                subsequence[0] = input[i];
+            } else if(input[i] > subsequence[length-1]) {
+                subsequence[length++] = input[i];
+            } else {
+                int replaceIndex = lowerBound(subsequence, 0, length, input[i]);
+                subsequence[replaceIndex] = input[i];
+            }
+        }
+
+        System.out.println(length);
+    }
+
+    public static int lowerBound(int[] arr, int fromIndex, int toIndex, int key) {
+        int start = fromIndex;
+        int end = toIndex - 1;
+
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if(arr[mid] < key) {
+                start = mid + 1;
+            } else if(arr[mid] > key) {
+                end = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return start;
+    }
+}
